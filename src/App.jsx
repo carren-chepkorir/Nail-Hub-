aac62a6c3e7a886ef2054e70a90c79d2b4eacc59
carren <chepkorircarren9@gmail.com>
2023-01-31 14:51:40 +0300

    Added routes

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './App.css'
-const App = () => {
-  return (
-    <div>Hello</div>
-  )
-}
-
-export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import Services from "./Routes/Services";
+import Contact from "./Routes/Contact";
+import About from "./Routes/About";
+import Home from "./Routes/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/services",
+    element: <Services />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+  {
+    path: "/About",
+    element: <About />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
